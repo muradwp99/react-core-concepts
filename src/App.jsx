1d12c728e6fa58148ac109148a8494f0f6c5a083
muradwp99 <muradujjaman05@gmail.com>
2024-05-10 15:05:02 +0600

    React Mapping

--- src/App.jsx
+++ src/App.jsx
@@ -5,6 +5,19 @@ import './App.css'
 import Todo from './Todo'
 
 function App() {
+
+  const singers = [
+    {
+      id: '1',
+      name: 'Kumar Sanu',
+      age: '22'
+    },
+    {
+      id:'2',
+      name: 'Kishore Kumar',
+      age: '25'
+    }
+  ]
   return (
     <>
       <div>
@@ -16,12 +29,27 @@ function App() {
         </a>
       </div>
       <h1>Vite + React</h1>
-      <Student></Student>
+      {/* <Student></Student>
       <Device name='Lenovo' price='$7000' ram='Corsair DDR4 8GB'
-      ></Device>
+      ></Device> */}
       <Todo task='Learn React' isDone={false}></Todo>
+
+       
+      {
+        singers.map(singer => <Singers key={singer.id} singer= {singer}></Singers>)
+      }
+      
+
     </>
   )
+}
+
+function Singers({singer}){
+  return <>
+      <h2>{singer.name}</h2>
+      <h4>Age: {singer.age}</h4>
+  
+  </>
 }
 
 // function Device(props){
@@ -35,35 +63,35 @@ function App() {
 //     </>
 //   )
 // }
-function Device({name, price, ram}){
+// function Device({name, price, ram}){
   
-  return(
-    <>
-      <h2>Name: {name}</h2>
-      <p>Price: {price}</p>
-      <p>Ram: {ram} </p>
+//   return(
+//     <>
+//       <h2>Name: {name}</h2>
+//       <p>Price: {price}</p>
+//       <p>Ram: {ram} </p>
     
-    </>
-  )
-}
+//     </>
+//   )
+// }
 
-function Student(){
+// function Student(){
 
-  const studentInfo = {
+//   const studentInfo = {
     
-      name: 'Murad',
-      id: 'MR14',
-      age: '26'
+//       name: 'Murad',
+//       id: 'MR14',
+//       age: '26'
     
-  }
+//   }
 
-  return (
-    <>
-      <h2>Hello My name is {studentInfo.name}</h2>
-      <h2>My Id is {studentInfo.id} & my Age is {studentInfo.age}</h2>
+//   return (
+//     <>
+//       <h2>Hello My name is {studentInfo.name}</h2>
+//       <h2>My Id is {studentInfo.id} & my Age is {studentInfo.age}</h2>
     
-    </>
-  )
-}
+//     </>
+//   )
+// }
 
 export default App
